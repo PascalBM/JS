91ac3aa161b5d11c0960b9a6afa867b7ceb16e52
Pascal B <mkno-@hotmail.com>
2021-10-14 17:00:36 +0200

    js done now jus html and css

--- ajax-pokedex/script.js
+++ ajax-pokedex/script.js
@@ -1,19 +1,40 @@
 
-let getData = async () => {
+let getData = () => {
     //took the api with the fetch
     let input = document.getElementById("input").value;
+    let pokeId = document.getElementById("pokeId");
     let image = document.getElementById("image");
     let name = document.getElementById("name");
-    let moves = document.getElementById("moves");
-    const nextBtn = document.getElementById("next");
-    const previousBtn = document.getElementById("previous");
+    let pokemoves = document.getElementById("pokemoves");
+    let evolution = document.getElementById("evolution");
+    const nextBtn = document.getElementById("nextBtn");
+    const previousBtn = document.getElementById("previousBtn");
 
-        await fetch('https://pokeapi.co/api/v2/pokemon/'+ input)
+
+        fetch('https://pokeapi.co/api/v2/pokemon/'+ input)
             .then(result => result.json())
             .then(data => {
-                name.textContent = data.name
+                pokeId.textContent = "ID-number:" + " " + data.id;
+                name.textContent = data.name;
                 console.log(data);
-                image.src = data.sprites["front_default"]
+                image.src = data.sprites["front_default"];
+                pokemoves.innerHTML="";
+                for (let i = 0; i < 4; i++) {
+                    pokemoves.innerHTML+= "<li>" + data.moves[i].move.name+ "</li>";
+                }
+                /*nextBtn.addEventListener("click", () => input+1);
+                previousBtn.addEventListener("click", () => input-1);*/
+                fetch('https://pokeapi.co/api/v2/pokemon-species/'+ input)
+                    .then(result => result.json())
+                    .then(data => {
+                    console.log(data.evolves_from_species.name)
+                       /*fetch('https://pokeapi.co/api/v2/pokemon/'+ data.evolves_from_species.name)
+                            .then(result => result.json())
+                            .then(data => {
+                                console.log(data)
+                                evolution.src = data.sprites["front_default"]
+                            })*/
+                    })
             })
 }
 
